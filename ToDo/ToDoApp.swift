0431409be95e8537a15f4d71d33046478d6325df
Melbin Mathew <mmathew@trl.co.uk>
2022-01-15 12:30:52 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Melbin Mathew on 15/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
